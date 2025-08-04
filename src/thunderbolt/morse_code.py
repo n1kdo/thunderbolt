2ffbbe0ab25cdfaea5236e3dd0dd31e8239bc563
n1kdo <otterson@gmail.com>
2025-08-04 12:07:04 -0400

    latest morse_code.py

--- src/thunderbolt/morse_code.py
+++ src/thunderbolt/morse_code.py
@@ -4,7 +4,7 @@
 
 __author__ = 'J. B. Otterson'
 __copyright__ = """
-Copyright 2022, 2024 J. B. Otterson N1KDO.
+Copyright 2022, 2024, 2025 J. B. Otterson N1KDO.
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
   1. Redistributions of source code must retain the above copyright notice, 
@@ -23,18 +23,18 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-__version__ = '0.9.3'
+__version__ = '0.9.5'
 
 # disable pylint import error
 # pylint: disable=E0401
 
+import asyncio
 import sys
+
 impl_name = sys.implementation.name
 if impl_name == 'cpython':
-    import asyncio
     import logging
 else:
-    import uasyncio as asyncio
     import micro_logging as logging
 
 
@@ -64,7 +64,7 @@ class MorseCode:
         'H': (MORSE_DIT, MORSE_DIT, MORSE_DIT, MORSE_DIT),
         'I': (MORSE_DIT, MORSE_DIT),
         'N': (MORSE_DAH, MORSE_DIT),
-        #  'O': (MORSE_DAH, MORSE_DAH, MORSE_DAH),
+        'O': (MORSE_DAH, MORSE_DAH, MORSE_DAH),
         'P': (MORSE_DIT, MORSE_DAH, MORSE_DAH, MORSE_DIT),
         'R': (MORSE_DIT, MORSE_DAH, MORSE_DIT),
         'S': (MORSE_DIT, MORSE_DIT, MORSE_DIT),
@@ -98,7 +98,7 @@ class MorseCode:
             for morse_letter in msg:
                 blink_pattern = patterns.get(morse_letter)
                 if blink_pattern is None:
-                    logging.debug(f'[MORSE_CODE] Warning: no pattern for letter {morse_letter}',
+                    logging.debug(f'No pattern for letter "{morse_letter}" ({ord(morse_letter)})',
                                   'morse_code:morse_sender')
                     blink_pattern = patterns.get(' ')
                 blink_list = list(blink_pattern)
